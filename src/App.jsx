cae1c0c5576e372429403c6568dfdee1d2b1e00e
kamrul25 <kamrul.coder01@gmail.com>
2026-09-17 20:43:30 +0600

    Decale project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Card Title</h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
+    <div className="min-h-screen flex flex-col bg-base-100 text-base-content font-sans">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<p>Hi kamrul</p>} />
+        <Route path="/about" element={<p>I am from about</p>} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
