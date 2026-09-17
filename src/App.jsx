9dd7dccacf1a61ee80e8de1b784127e98f621d57
kamrul25 <kamrul.coder01@gmail.com>
2026-09-17 23:52:52 +0600

    Routing and define home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import MovieListingPage from "./pages/MovieListingPage";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-base-100 text-base-content font-sans">
       <Navbar />
       <Routes>
-        <Route path="/" element={<p>Hi kamrul</p>} />
-        <Route path="/about" element={<p>I am from about</p>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MovieListingPage />} />
       </Routes>
       <Footer />
     </div>
