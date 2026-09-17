d0075aec9f3456cb4c9742d9a5c4d85caf874205
kamrul25 <kamrul.coder01@gmail.com>
2026-09-17 23:05:32 +0600

    Decalre Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
-const Footer =()=>{
-    return (<h2>I am from footer</h2>);
-}
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-6 bg-base-300 text-base-content mt-auto">
+      <aside>
+        <p className="font-bold text-lg">MovieExplorer</p>
+        <p>© 2026 MovieExplorer. All rights reserved.</p>
+      </aside>
+    </footer>
+  );
+};
 
 export default Footer;
